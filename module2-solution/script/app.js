3551b85cc3c241f20c2e48a4b6b7200792a6bb31
Christopher Tabis <ctabis@3amlabs.net>
2017-08-01 14:14:32 -0400

    in progress. broken though

--- module2-solution/script/app.js
+++ module2-solution/script/app.js
@@ -6,9 +6,14 @@
 angular.module('ShoppingCartApp', [])
 .controller('ToBuyListController', ToBuyListController)
 .controller('AlreadyBoughtListController', AlreadyBoughtListController)
+.service('ListService', ListService);
 
-ToBuyListController.$inject = ['$scope']; //don't need to inject $scope?
-function ToBuyListController ($scope){ //and here too?
+ToBuyListController.$inject = ['ListService'];
+function ToBuyListController (ListService){
+
+  var list1 = this;
+
+  //var shoppingCart = ListService();
 
   // -the ToBuyList needs to start pre-populated with 5 itmes.
   // -need functinality to add items to the list?
@@ -16,11 +21,73 @@ function ToBuyListController ($scope){ //and here too?
 
   // so, we're just using the controller to instantiate a list? The functionality of the list in implemented in a service?
 
+  // what does this controller do and need in order to function?
+  //
+  // it displays the array of objects that need to be bought. Per the assignment instructions, this array can be hard coded to a variable
+  //
+  // there needs to be a service that can remove items from the array, and push those to list2
+  list1.items = ListService.getItemsDefault();
+
+  list1.removeItems = function (itemIndex) { //this is defining the function in the HTML binding list.removeItems()
+    var itemToMove = ListService.removeItemsDefault(itemIndex);
+
+    // ListService.addItem (itemToMove.itemName, itemToMove.itemQuantity);
+    ListService.addItem (itemToMove);
+
+    console.log(itemToMove);
+    console.log(itemToMove.quantity);
+  };
+}
+
+AlreadyBoughtListController.$inject = ['ListService'];
+function AlreadyBoughtListController (ListService){
+
+  var list2 = this;
+
+  list2.items = ListService.getItems();
+
 };
 
-AlreadyBoughtListController.$inject = ['$scope']
-function AlreadyBoughtListController ($scope){
+function ListService () {
+  //this service defines the shopping list and functionality
+
+  var service = this;
+
+  var items = []; //empty array
+
+  var itemsDefault = [{name: "poop", quantity: 3}, {name: "poop", quantity: 3}, {name: "poop", quantity: 3}, {name: "poop", quantity: 3}, {name: "poop", quantity: 3}];
+
+  // service.addItem = function (itemName, itemQuantity) {
+  //   var item = {
+  //     name: itemName,
+  //     quantity: itemQuantity
+  //   };
+  //   items.push(item);
+  // };
+
+  service.addItem = function (item) {
+
+    items.push(item);
+  };
+
+  service.removeItemsDefault = function (itemIndex){
+
+    var removed = itemsDefault.splice(itemIndex, 1);
+
+    return removed;
+  };
+
+  // service.removeItems = function () {
+  //
+  // };
+
+  service.getItemsDefault = function () {
+    return itemsDefault;
+  };
+
+  service.getItems = function () {
+    return items;
+  };
 
 };
-
 })();
