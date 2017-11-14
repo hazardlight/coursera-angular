abea4f8748687729290457e51629c6005c66f513
Christopher Tabis <ctabis@3amlabs.net>
2017-11-14 12:34:02 -0500

    assingment finished

--- module4-solution/src/routes.js
+++ module4-solution/src/routes.js
@@ -25,16 +25,26 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
     templateUrl: 'src/restaurant/templates/categories.template.html',
     controller: 'CategoriesController as vm',
     resolve: {
-      categories: ['MenuService', function (MenuService) { //sooo this is where you get 'items'
+      categories: ['MenuService', function (MenuService) { //sooo this is where you get 'categories', which is injected into the controller
         return MenuService.getAllCategories();
       }]
     }
   })
-
   .state('categories.items', {
     url: '/items/{itemId}',
     templateUrl: 'src/restaurant/templates/items.template.html',
-    controller: "ItemsController as itmCtrl"
+    controller: "ItemsController as vm"
+    // ,
+    // resolve: {
+    //   itemsInCategory: ['MenuService', function (MenuService) { //sooo this is where you get 'categories', which is injected into the controller
+    //     // var categoryShortName;
+    //     return MenuService.getItemsForCategory(categoryShortName); //how do we pass a value into getItemsForCategory() at the state change? should we even do this here, or in the controller?
+    //   }]
+    // }
+  // .state('categories.items', {
+  //   url: '/items/{itemId}',
+  //   templateUrl: 'src/restaurant/templates/items.template.html',
+  //   controller: "ItemsController as vm"
   });
 
 }
