26c063eaa8185d897a6191b0ad1c46ffa0615e35
Christopher Tabis <ctabis@3amlabs.net>
2018-01-29 17:26:43 -0500

    adding more stuff

---
+++ module5-solution/src/common/sign-up.service.js
@@ -0,0 +1,13 @@
+(function () {
+"use strict";
+
+angular.module('common')
+.service('SignUpService', SignUpService);
+
+
+SignUpService.$inject = ['$http', 'ApiPath'];
+function SignUpService($http, ApiPath) {
+  var service = this;
+
+}
+})();
--- module5-solution/src/public/sign-up/sign-up.controller.js
+++ module5-solution/src/public/sign-up/sign-up.controller.js
@@ -4,10 +4,19 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-MenuItemsController.$inject = [];
+SignUpController.$inject = [];
 function SignUpController() {
   var $ctrl = this;
   //$ctrl.menuItems = menuItems;
+  $ctrl.firstName;
+  $ctrl.lastName;
+  $ctrl.email;
+  $ctrl.telephone;
+  $ctrl.favorite;
+
+  $ctrl.signUp = function (){
+    
+  }
 }
 
 })();
